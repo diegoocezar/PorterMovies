39ae0dc024a7744f6c80a77a70600580632b1a48
diegoocezar <diego.bernardino@amedigital.com>
2021-04-28 11:58:17 -0300

    create movieList component

---
+++ src/components/MoviesList/index.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { View, FlatList, Dimensions } from 'react-native';
+import MovieItem from './parts/MovieItem';
+import { getMovies } from '../../services/api';
+
+const {width, height} = Dimensions.get('window')
+
+const MoviesList = () => {
+
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const movies = await getMovies();
+      setMovies(movies.data);
+    };
+
+    fetchData();
+
+  }, []);
+
+  return (
+    <FlatList
+    showsHorizontalScrollIndicator={false}
+    data={movies}
+    keyExtractor={(item) => item.key}
+    horizontal
+    contentContainerStyle={{ alignItems: 'center' }}
+    renderItem={({ item, index }) => {
+
+      return (
+        <View style={{ width: width }}>
+          <MovieItem movie={item}/>
+        </View>
+      );
+    }}
+  />
+  );
+}
+
+export default MoviesList;
