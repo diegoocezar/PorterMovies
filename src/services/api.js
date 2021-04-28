d7d976545f9eaabbd0c81aa38f3b8ec6bd8210ab
diegoocezar <diego.bernardino@amedigital.com>
2021-04-28 01:55:26 -0300

    create service file

---
+++ src/services/api.js
@@ -0,0 +1,58 @@
+const API_KEY = '7ff9651aec389cba32ebbfccc4cdebc4';
+const genres = {
+  12: 'Adventure',
+  14: 'Fantasy',
+  16: 'Animation',
+  18: 'Drama',
+  27: 'Horror',
+  28: 'Action',
+  35: 'Comedy',
+  36: 'History',
+  37: 'Western',
+  53: 'Thriller',
+  80: 'Crime',
+  99: 'Documentary',
+  878: 'Science Fiction',
+  9648: 'Mystery',
+  10402: 'Music',
+  10749: 'Romance',
+  10751: 'Family',
+  10752: 'War',
+  10770: 'TV Movie',
+};
+
+const getImagePath = (path) =>
+  `https://image.tmdb.org/t/p/w440_and_h660_face${path}`;
+const getBackdropPath = (path) =>
+  `https://image.tmdb.org/t/p/w370_and_h556_multi_faces${path}`;
+
+export const getMovies = async (page = 1) => {
+
+  const API_URL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=${page}`;
+  const fetchData = await fetch(API_URL).then((resp) => resp.json());
+  const movies = fetchData.results.map(
+    ({
+      id,
+      original_title,
+      poster_path,
+      backdrop_path,
+      vote_average,
+      overview,
+      release_date,
+      genre_ids
+    }) => ({
+      key: String(id),
+      title: original_title,
+      poster: getImagePath(poster_path),
+      backdrop: getBackdropPath(backdrop_path),
+      rating: vote_average,
+      description: overview,
+      releaseDate: release_date,
+      genres: genre_ids.map((genre) => genres[genre]),
+
+    })
+  );
+  // totalPages: fetchData.total_pages,
+
+  return movies;
+};
