7453159c61bd7ed2fb5b182f327c2100d7e297d5
diegoocezar <diego.bernardino@amedigital.com>
2021-05-02 23:01:20 -0300

    add color prop

--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -4,7 +4,7 @@ import { FontAwesome } from '@expo/vector-icons';
 
 import styles from './styles';
 
-const Rating = ({rating}) => {
+const Rating = ({rating, color}) => {
   const stars = Math.floor(rating / 2);
 
   const filledStars = Array(stars).fill('star');
@@ -12,12 +12,12 @@ const Rating = ({rating}) => {
 
   return (
     <View style={styles.rating}>
-      <Text style={styles.ratingNumber}>{rating}</Text>
+      <Text style={[styles.ratingNumber, color && {color: color}]}>{rating}</Text>
       {filledStars.map((icon, index) => {
         return <FontAwesome key={index} name={icon} size={12} color={rating > 6 ? 'green' : 'red'} />;
       })}
       {emptyStars.map((icon, index) => {
-        return <FontAwesome key={index} name={icon} size={12} color="gray" />;
+        return <FontAwesome key={index} name={icon} size={12} color={color ? color : "gray"} />;
       })}
     </View>
   );
